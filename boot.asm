9a70c9cab236232412e6093738774e1d6ebe4d1a
Skeome <skeome@gmail.com>
2025-11-17 14:46:05 -0800

    Update

--- boot.asm
+++ boot.asm
@@ -1,53 +1,66 @@
-; boot.asm - Stage 1 (Minimal LBA Read)
+; boot.asm - Stage 1 (Minimal LBA Read - Drive ID Fixed)
 ; Job: Load kernel.bin (Stage 2) into memory at 0x1000 using LBA.
 ;-----------------------------------------------------------------------------
 
 [ORG 0x7C00]
 [BITS 16]
 
-KERNEL_START_LBA        equ 0x01      ; LBA 1 (Sector 2 on disk)
-KERNEL_SECTORS_TO_READ  equ 40        ; 40 sectors (20KB for kernel.asm)
-KERNEL_LOAD_SEGMENT     equ 0x0100    ; Physical address 0x1000
+KERNEL_START_LBA        equ 0x01      ; LBA 1
+KERNEL_SECTORS_TO_READ  equ 40        
+KERNEL_LOAD_SEGMENT     equ 0x0100    ; ES = 0x0100 (Physical address 0x1000)
 KERNEL_LOAD_OFFSET      equ 0x0000    
+
+; Disk Address Packet (DAP) definition placed early to stabilize offsets
+dap_packet:
+dap_size:           db 0x10         ; 0x00: Packet size
+dap_reserved:       db 0x00         ; 0x01
+dap_sectors:        dw KERNEL_SECTORS_TO_READ ; 0x02: Sectors to read (40)
+dap_buffer_offset:  dw KERNEL_LOAD_OFFSET     ; 0x04: Load offset (0x0000)
+dap_buffer_segment: dw KERNEL_LOAD_SEGMENT    ; 0x06: Load segment (0x0100)
+dap_lba_low:        dd KERNEL_START_LBA       ; 0x08: LBA Start (1)
+dap_lba_high:       dd 0x00000000             ; 0x0C
+
+; Padding and Drive ID Storage
+times 510 - ($ - $$) - 1 db 0 
+boot_drive_storage: db 0        ; Store drive ID here (at 0x7DFD)
+dw 0xAA55                       ; Boot signature
+
 
 start:
     cli                     ; Disable interrupts
     
     ; 1. Setup Segments and Stack
     xor ax, ax              
-    mov ds, ax              ; DS=0
-    mov es, ax              ; ES=0
-    mov ss, ax              ; SS=0
-    mov sp, 0x7C00          ; Stack starts just below 0x7C00 (grows down)
+    mov ds, ax              ; DS=0 for accessing DAP
+    mov es, ax              
+    mov ss, ax              
+    mov sp, 0x7C00          ; Stack starts just below 0x7C00
 
-    ; Save boot drive ID (DL) to 0x7DFD
+    ; Save boot drive ID (DL)
     mov [boot_drive_storage], dl
     
     sti                     ; Re-enable interrupts
 
-    ; 2. LBA Read Setup
+    ; 2. Execute LBA Read (INT 0x13 / AH=0x42)
     
     ; Setup Buffer Address (ES:BX = 0x0100:0x0000 -> 0x1000 physical)
     mov ax, KERNEL_LOAD_SEGMENT
     mov es, ax
     mov bx, KERNEL_LOAD_OFFSET
     
-    ; Setup Disk Address Packet (DAP) at DS:SI (0x0000:0x7C98 in low memory)
+    ; Setup DS:SI to point to the DAP structure (0x0000:dap_packet)
     mov si, dap_packet
     
-    ; Populate DAP with target values
-    mov word [si + 0x02], KERNEL_SECTORS_TO_READ ; dap_sectors (40)
-    mov word [si + 0x04], bx                     ; dap_buffer_offset (0)
-    mov word [si + 0x06], es                     ; dap_buffer_segment (0x0100)
-    mov dword [si + 0x08], KERNEL_START_LBA      ; dap_lba_low (1)
-    
-    ; 3. Execute LBA Read (INT 0x13 / AH=0x42)
     mov ah, 0x42            ; Extended Read Sectors
-    mov dl, [boot_drive_storage] ; Load original drive ID (usually 0x00)
     
-    mov dl, 0x80            ; CRITICAL WORKAROUND: Use Hard Disk ID for LBA read in QEMU
+    ; --- The Fix: Temporarily use the Hard Disk ID for the AH=0x42 call ---
+    ; --- We trust that the floppy BIOS should handle the ID conversion internally. ---
+    mov dl, 0x80
     
     int 0x13
+    
+    ; Check drive ID again (in case BIOS reset it)
+    mov dl, [boot_drive_storage] 
     
     jnc .jump_to_stage2     ; If carry flag is clear, success!
     
@@ -58,32 +71,13 @@ start:
     
 .jump_to_stage2:
     
-    ; 4. Final Segment Setup (CRITICAL FIXES)
+    ; 3. Final Segment Setup (CRITICAL FIXES)
     ; DS and ES must match the new code segment (0x0100) before the jump.
     mov ax, KERNEL_LOAD_SEGMENT
     mov ds, ax              ; Set DS to 0x0100
     mov es, ax              ; Set ES to 0x0100
 
-    ; 5. Jump to the loaded kernel (Stage 2)
+    ; 4. Jump to the loaded kernel (Stage 2)
     push KERNEL_LOAD_SEGMENT ; Pushes 0x0100 (CS) onto the stack
     push KERNEL_LOAD_OFFSET  ; Pushes 0x0000 (IP) onto the stack
     retf                     ; Far Return/Jump to 0x0100:0x0000
-
-; --------------------------------------
-; Data and Padding
-; --------------------------------------
-
-; Disk Address Packet (DAP) Structure for AH=0x42 (16 bytes)
-dap_packet:
-dap_size:           db 0x10         ; 0x00 (Size)
-dap_reserved:       db 0x00         ; 0x01
-dap_sectors:        dw 0x0000       ; 0x02 (Filled by code)
-dap_buffer_offset:  dw 0x0000       ; 0x04 (Filled by code)
-dap_buffer_segment: dw 0x0000       ; 0x06 (Filled by code)
-dap_lba_low:        dd 0x00000000   ; 0x08 (Filled by code)
-dap_lba_high:       dd 0x00000000   ; 0x0C
-
-; Padding and Signature
-times 510 - ($ - $$) - 1 db 0 
-boot_drive_storage: db 0        ; Store drive ID here (at 0x7DFD)
-dw 0xAA55                       ; Boot signature
